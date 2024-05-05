39fefbfaeae9de5e6d36c372a339a4901c268efd
Pian Pawakapan <pianpwk@meta.com>
2024-05-04 17:37:21 -0700

    [export] handle root swapping in derived dims suggested fixes
    Summary:
    The current suggested fixes for derived dims can lead to root dims/derived dims being swapped, e.g. `dy - 1, dy` -> `dx, dx + 1`. This leads to problematic suggested fixes that look like "dy - 1 = Dim("dy - 1") since we don't have access to the original variable name.
    
    This PR only adds a suggested fix for the root dim, and removes all other derived suggestions.
    
    For example, with the export test case test_derived_dim_out_of_order_simplified:
    ```
    _dimz = torch.export.Dim("_dimz", min=6, max=8)
    dimy = _dimz - 1
    dimx = dimy - 1
    dimz = torch.export.Dim("dimz", min=6, max=8)  # doesn't work, should be = _dimz
    
    class Foo(torch.nn.Module):
        def forward(self, x, y, z):
            return x + y[1:] + z[2:]
    
    foo = Foo()
    u, v, w = torch.randn(5), torch.randn(6), torch.randn(7)
    export(
        foo,
        (u, v, w),
        dynamic_shapes=({0: dimx}, {0: dimy}, {0: dimz}),
    )
    ```
    
    Before:
    ```
    Suggested fixes:
      _dimz = Dim('_dimz', min=3, max=9223372036854775807)  # 2 <= _dimz - 1 <= 9223372036854775806
      _dimz - 2 = Dim('_dimz - 2', min=4, max=6)
      _dimz = Dim('_dimz', min=2, max=9223372036854775806)  # 2 <= _dimz <= 9223372036854775806
      _dimz - 1 = _dimz - 1
      dimz = _dimz
    ```
    
    New suggested fixes:
    ```
    Suggested fixes:
      dimz = _dimz
    ```
    
    Test Plan: export test cases
    
    Differential Revision: D56974543

--- torch/_export/non_strict_utils.py
+++ torch/_export/non_strict_utils.py
@@ -200,6 +200,7 @@ def _flatten_dynamic_shapes(
 def produce_guards_and_solve_constraints(
     fake_mode: FakeTensorMode,
     gm: torch.fx.GraphModule,
+    dynamic_shapes: Union[Dict[str, Any], Tuple[Any], List[Any]],
     equalities_inputs: EqualityConstraint,
     original_signature: inspect.Signature,
 ):
@@ -243,7 +244,10 @@ def produce_guards_and_solve_constraints(
     dim_constraints.remove_redundant_dynamic_results()
     forced_specializations = dim_constraints.forced_specializations()
     msg = dim_constraints.prettify_results(
-        original_signature, constraint_violation_error, forced_specializations
+        original_signature,
+        dynamic_shapes,
+        constraint_violation_error,
+        forced_specializations,
     )
     if constraint_violation_error:
         constraint_violation_error.args = (constraint_violation_error.args[0] + msg,)
